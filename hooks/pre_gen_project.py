781737b1d4733b67397893d71004b86b0caacb62
dsul <dsul@users.noreply.github.com>
2019-03-07 15:28:45 -0500

    WIP: Modify pre-hook to utilize virtualenv

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,9 +1,22 @@
-import conda.cli
 import subprocess
 
-# Install opencv3 & numpy
-conda.cli.main('conda', 'install', '-y', 'opencv')
+'''
+Assumes that python and pip is installed and that
+pyenv has been installed with the installer:
+https://github.com/pyenv/pyenv-installer
+'''
 
-# Install pytest-watch
+directory_name = '{{ cookiecutter.directory_name }}'
+virtual_env_name = directory_name + '-env-3.6.8'
+
+# Environment setup
+subprocess.run(["pyenv", "install", "3.6.8"])
+subprocess.run(["pyenv", "virtualenv", "3.6.8", virtual_env_name])
+subprocess.run(["pyenv", "activate", virtual_env_name])
+subprocess.run(["pyenv", "local", virtual_env_name], cwd='./' + directory_name)
+
+# Install dependencies
 subprocess.run(["pip", "install", "pytest-watch"])
-
+subprocess.run(["pip", "install", "opencv-python"])
+subprocess.run(["pip", "install", "numpy"])
+subprocess.run(["pip", "freeze", ">", "requirements.txt"], cwd='./' + directory_name)
